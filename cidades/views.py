11de9ee796068c6b18b9911799b933edb6f538bc
SilvaIgorDosSantos <silvaigordossantos@gmail.com>
2021-04-24 14:31:46 -0300

    Create index function for example response

--- cidades/views.py
+++ cidades/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from cidades.models import City
 
 # Create your views here.
+def index(request):
+    if request.method == "GET":
+        cidade = City.objects.get(nome="São Paulo")
+        cidades = {
+            "suggestions": [cidade.serialize(1), cidade.serialize(0.98)]
+        }
+        return JsonResponse(cidades, json_dumps_params={'ensure_ascii': False}, status=200)
+    else:
+        return JsonResponse({"error": "GET request required."}, status=400)
