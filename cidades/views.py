fc767293dcbdb77434f9a6677d305d650a7e5ef9
SilvaIgorDosSantos <silvaigordossantos@gmail.com>
2021-04-25 17:37:38 -0300

    Edit suggestions endpoint with searchs

--- cidades/views.py
+++ cidades/views.py
@@ -1,13 +1,15 @@
 import json
 from django.http import JsonResponse
 from cidades.models import City
+from cidades.services.city_search import search
 
 # Create your views here.
 def index(request):
     if request.method == "GET":
-        cidade = City.objects.get(nome="São Paulo")
+        search_entry = request.GET.get('q')
+        suggestions = search(search_entry)
         cidades = {
-            "suggestions": [cidade.serialize(1), cidade.serialize(0.98)]
+            "suggestions":  suggestions
         }
         return JsonResponse(cidades, json_dumps_params={'ensure_ascii': False}, status=200)
     else:
