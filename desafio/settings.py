52a722d2f429b5ce7e4a87d95383b133532afd30
SilvaIgorDosSantos <silvaigordossantos@gmail.com>
2021-04-26 01:31:04 -0300

    Fix location for heroku import

--- desafio/settings.py
+++ desafio/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,4 +126,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+if os.getenv("HEROKU", False):
+    import django_heroku
+    django_heroku.settings(locals())
